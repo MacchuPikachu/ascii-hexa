e401b17740737eb375b7c431a8801270dab35cc4
Jan Macháček <jan.machacek.020@pslib.cz>
2022-02-07 13:24:40 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main() {
+	FILE* soubor = fopen("vystup.txt", "w");
+
+	if (soubor == NULL) {
+		printf("ERROR: Soubor nelze otevrit\n");
+		return 1;
+	}
+
+	int ascii = 0x40;
+
+	fprintf(soubor, "ASCII | HEXADECIMALNI\n");
+
+	for (int i = 0; i < 26; i++) {
+		ascii += 1;
+		fwrite(&ascii, sizeof(int), 1, soubor);
+		fprintf(soubor, "0x%x\n", ascii);
+	}
+
+	if (fclose(soubor) == EOF) {
+		printf("ERROR: Soubor se nepodarilo otevrit\n");
+		return 1;
+	}
+
+	return(0);
+}
